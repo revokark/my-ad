31941f0b8393207f300fd672196507d21f0ae33e
Rea Krakover <rea.krakover@gmail.com>
2022-01-30 23:47:16 +0200

    update veriosn

--- facebook/aws.user.js
+++ facebook/aws.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://m.facebook.com/
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 1/30/2022, 9:45:28 PM
 // ==/UserScript==
